b5cb4b4e497dab610da8ef8a56acc4d064a19d10
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-10-19 10:42:42 +0200

    added posts page example

--- templates/posts.php
+++ templates/posts.php
@@ -1,9 +1,23 @@
 <?php require_once 'includes/header.php' ?>
 
-<?php
+<div class="container">
+    <div class="row">
+        <div class="card text-center mt-5">
+        <div class="card-header">
+            Featured
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        <div class="card-footer text-muted">
+            2 days ago
+        </div>
+        </div>
+    </div>
+</div>
 
 
-
-?>
 
 <?php require_once 'includes/footer.php' ?>
